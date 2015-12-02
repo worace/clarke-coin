fd5170c84f778694765180a1bfeb3247d01e5ad3
Horace Williams <horace.d.williams@gmail.com>
2015-12-01 17:51:26 -0700

    try example with decoding pub key from string

--- src/block_chain/wallet.clj
+++ src/block_chain/wallet.clj
@@ -17,20 +17,28 @@
  (org.bouncycastle.jce.provider.BouncyCastleProvider.))
 
 (defn decode64 [str]
-  (javax.xml.bind.DatatypeConverter/parseBase64Binary str))
+  (.decode (java.util.Base64/getDecoder) str))
 
-(defn keydata [filepath]
- (->> filepath
-      (io/reader)
+(defn encode64 [bytes]
+  (.encodeToString (java.util.Base64/getEncoder) bytes))
+
+(defn keydata [reader]
+ (->> reader
       (PEMParser.)
       (.readObject)))
 
 (defn read-pem-private-key [filepath]
-  (let [kd (keydata filepath)]
+  (let [kd (keydata (io/reader filepath))]
     (.getKeyPair (org.bouncycastle.openssl.jcajce.JcaPEMKeyConverter.) kd)))
 
 (defn read-pem-public-key [filepath]
-  (let [kd (keydata filepath)
+  (let [kd (keydata (io/reader filepath))
+        kf (KeyFactory/getInstance "RSA")
+        spec (java.security.spec.X509EncodedKeySpec. (.getEncoded kd))]
+    (.generatePublic kf spec)))
+
+(defn read-pem-public-key-from-string [string]
+  (let [kd (keydata (io/reader (.getBytes string)))
         kf (KeyFactory/getInstance "RSA")
         spec (java.security.spec.X509EncodedKeySpec. (.getEncoded kd))]
     (.generatePublic kf spec)))
@@ -72,4 +80,18 @@
 (let [encrypted (encrypt (.getBytes "Pizza") public-key)]
   (println "decryted: " (map char (decrypt encrypted (.getPrivate private-key)))))
 
+(def message "jFd5MRjGs3S19XIblzdkVkEwUc7A7WRPdWtj6JPXuqU6k/ue1sLRLRhc0inu\nNUglbx3TJTcq6i2FvIA1wb7LnMfw9MiFVs/wbxhtQFmx9VYKHLQq7pqKiYkZ\n6Mqtr1SiUT5e6OFc+6WEIj6GzaF6LRU9osGmUQs40iPQr5016XafRCDeIval\nNo8na5C2WZu4m4ZYadnOwDkAbuk4Vhd7xxR+F433Qitncux8oKHhVOpR9yYV\n7JzNVPzC5o+fX8PNns39Pcb91m3Z243GOl8xlfBFPYa0Wytd9DJB13MYAiWQ\n6gAGUa3kYJJIqMQU3Yyz0DCvRtoJiFXacrJoAvQ5zg==")
 
+(def sample-pub-key-string
+  "-----BEGIN PUBLIC KEY-----
+MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDGU3lYomZqoluRGcM76VPYALvY
+Asel+CUivvKK6A6NUf9bWrqU3GsNi2biHzn7JrIaTxeEu0vw7MFi1dd5nxzuI/ow
+f0vyp+SSDGQ/EuSgrFwIZowHD/9QFMMiFZbqCxPzvxKYljPE5BsCLI7sWwotseQ1
+iUxRHiIhvSo/3md7XwIDAQAB
+-----END PUBLIC KEY-----
+")
+
+(let [pubkey (read-pem-public-key-from-string sample-pub-key-string)
+      encrypted (encrypt (.getBytes "Hi Josh!") pubkey)]
+  (println "Encrypted 'Hi Josh!':")
+  (println (encode64 encrypted)))
