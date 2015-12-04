bd4b418002d46d50dd6e59b466b76db4ba2fc844
Horace Williams <horace.d.williams@gmail.com>
2015-12-04 12:52:10 -0700

    refactoring and documenting
    finishing up test cases for basic RSA methods
    adding docstrings for main methods
    pruning and refactoring the namespace

--- src/block_chain/wallet.clj
+++ src/block_chain/wallet.clj
@@ -1,82 +1,90 @@
 (ns block-chain.wallet
   (:require [clojure.java.io :as io]
-            [clojure.string :refer [join split]])
-  (:import
-    (org.bouncycastle.openpgp
-     PGPUtil)
-    (org.bouncycastle.openssl
-     PEMParser)
-    (java.security
-     KeyFactory)
-    (java.security.spec
-     PKCS8EncodedKeySpec)))
+            [clojure.string :refer [join split]]))
 
 ;; Thanks to http://nakkaya.com/2012/10/28/public-key-cryptography/
-;; for most of these snippets
+;; for many of these snippets
 
-(java.security.Security/addProvider
- (org.bouncycastle.jce.provider.BouncyCastleProvider.))
+(java.security.Security/addProvider (org.bouncycastle.jce.provider.BouncyCastleProvider.))
 
-(defn decode64 [str]
-  (.decode (java.util.Base64/getDecoder) str))
+(defn decode64 [str] (.decode (java.util.Base64/getDecoder) str))
 
-(defn encode64 [bytes]
-  (.encodeToString (java.util.Base64/getEncoder) bytes))
+(defn encode64 [bytes] (.encodeToString (java.util.Base64/getEncoder) bytes))
 
 (defn keydata [reader]
  (->> reader
-      (PEMParser.)
+      (org.bouncycastle.openssl.PEMParser.)
       (.readObject)))
 
 (defn pem-string->key-pair [string]
+  "Convert a PEM-formatted private key string to a public/private keypair.
+   Returns java.security.KeyPair."
   (let [kd (keydata (io/reader (.getBytes string)))]
     (.getKeyPair (org.bouncycastle.openssl.jcajce.JcaPEMKeyConverter.) kd)))
 
 (defn pem-string->pub-key [string]
+  "Convert a PEM-formatted public key string to an RSA public key.
+   Returns sun.security.rsa.RSAPublicKeyImpl"
   (let [kd (keydata (io/reader (.getBytes string)))
-        kf (KeyFactory/getInstance "RSA")
+        kf (java.security.KeyFactory/getInstance "RSA")
         spec (java.security.spec.X509EncodedKeySpec. (.getEncoded kd))]
     (.generatePublic kf spec)))
 
 (defn pem-file->public-key [filepath]
+  "Read a file containing a PEM-formatted public key
+   and return a matching key"
   (pem-string->pub-key (slurp filepath)))
 
 (defn pem-file->key-pair [filepath]
-  (pem-string->private-key (slurp filepath)))
+  "Read a file containing a PEM-formatted private key and
+   return a matching keypair"
+  (pem-string->key-pair (slurp filepath)))
 
 (defn format-pem-string [encoded key-type]
-  (let [formatted (join "\n"
-                        (map #(apply str %)
-                             (partition 64 64 [] encoded)))]
-    (str "-----BEGIN "
-         key-type
-         "-----\n"
+  "Takes a Base64-encoded string of key data and formats it
+   for file-output following openssl's convention of wrapping lines
+   at 64 characters and appending the appropriate header and footer for
+   the specified key type"
+  (let [chunked (->> encoded
+                     (partition 64 64 [])
+                     (map #(apply str %)))
+        formatted (join "\n" chunked)]
+    (str "-----BEGIN " key-type "-----\n"
          formatted
-         "\n-----END "
-         key-type
-         "-----\n")))
+         "\n-----END " key-type "-----\n")))
 
 (defn private-key->pem-string [key]
-  (format-pem-string
-   (encode64
-    (.getEncoded
-     (.toASN1Primitive
-      (.parsePrivateKey
-       (org.bouncycastle.asn1.pkcs.PrivateKeyInfo/getInstance (.getEncoded key))))))
-   "RSA PRIVATE KEY"))
+  "Convert RSA private keypair to a formatted PEM string for saving in
+   a .pem file. By default these private keys will encode themselves as PKCS#8
+   data (e.g. when calling (.getEncoded private-key)), so we have to convert it
+   to ASN1, which PEM uses (this seems to also be referred to as PKCS#1).
+   More info here http://stackoverflow.com/questions/7611383/generating-rsa-keys-in-pkcs1-format-in-java"
+  (-> (.getEncoded key)
+      (org.bouncycastle.asn1.pkcs.PrivateKeyInfo/getInstance)
+      (.parsePrivateKey)
+      (.toASN1Primitive)
+      (.getEncoded)
+      (encode64)
+      (format-pem-string "RSA PRIVATE KEY")))
 
 (defn public-key->pem-string [key]
+  "Generate PEM-formatted string for a public key. This is simply a base64
+   encoding of the key wrapped with the appropriate header and footer."
   (format-pem-string (encode64 (.getEncoded key))
                      "PUBLIC KEY"))
 
-(defn kp-generator []
+(defn kp-generator [length]
   (doto (java.security.KeyPairGenerator/getInstance "RSA" "BC")
-    (.initialize 2048)))
+    (.initialize length)))
 
-(defn generate-keypair []
-  (.generateKeyPair (kp-generator)))
+(defn generate-keypair
+  "Generate an RSA Keypair. Accepts optional length. Default key length is 2048."
+  ([] (generate-keypair 2048))
+  ([length] (.generateKeyPair (kp-generator length))))
 
 (defn encrypt [message public-key]
+  "Perform RSA public key encryption of the given message (as a string).
+   Returns a Base64 encoded string of the encrypted data."
   (encode64
    (let [cipher (doto (javax.crypto.Cipher/getInstance "RSA/ECB/PKCS1Padding" "BC")
                   (.init javax.crypto.Cipher/ENCRYPT_MODE public-key))]
@@ -84,52 +92,30 @@
 
 
 (defn decrypt [message private-key]
+  "RSA private key decryption of an encrypted message. Expects a base64-encoded string
+   of the encrypted data."
   (apply str
          (map char (let [cipher (doto (javax.crypto.Cipher/getInstance "RSA/ECB/PKCS1Padding" "BC")
                                   (.init javax.crypto.Cipher/DECRYPT_MODE private-key))]
                      (.doFinal cipher (decode64 message))))))
 
-(defn sign [data private-key]
-  (let [sig (doto (java.security.Signature/getInstance "SHA1withRSA" "BC")
-              (.initSign private-key (java.security.SecureRandom.))
-              (.update data))]
-    (.sign sig)))
+(defn sign [message private-key]
+  "RSA private key signing of a message. Takes message as string"
+  (encode64
+   (let [msg-data (.getBytes message)
+         sig (doto (java.security.Signature/getInstance "SHA256withRSA" "BC")
+               (.initSign private-key (java.security.SecureRandom.))
+               (.update msg-data))]
+     (.sign sig))))
 
-(defn verify [signature data public-key]
-  (let [sig (doto (java.security.Signature/getInstance "SHA1withRSA" "BC")
+(defn verify [encoded-sig message public-key]
+  "RSA public key verification of a signature. Takes signature as base64-encoded string
+   of signature data and message as a string. Returns true/false if signature
+   is valid."
+  (let [msg-data (.getBytes message)
+        signature (decode64 encoded-sig)
+        sig (doto (java.security.Signature/getInstance "SHA256withRSA" "BC")
               (.initVerify public-key)
-              (.update data))]
+              (.update msg-data))]
     (.verify sig signature)))
 
-;; (def keypair (generate-keypair))
-;; (def private-key (read-pem-private-key "/Users/worace/Desktop/keys/private_key.pem"))
-;; (def public-key (read-pem-public-key "/Users/worace/Desktop/keys/public_key.pem"))
-
-#_(let [encrypted (encrypt "Pizza" public-key)]
-  (println "decryted: " (decrypt encrypted (.getPrivate private-key))))
-
-#_(def keypair (pem-file->private-key "./test/sample_private_key.pem"))
-
-#_(encode64 (.getEncoded (.toASN1Primitive (.parsePrivateKey (org.bouncycastle.asn1.pkcs.PrivateKeyInfo/getInstance (.getEncoded (.getPrivate keypair)))))))
-
-#_(let [k (.getPrivate (generate-keypair))
-      w (java.io.StringWriter.)
-      po (org.bouncycastle.openssl.jcajce.JcaMiscPEMGenerator.
-          k)
-      pem (org.bouncycastle.util.io.pem.PemWriter. w)]
-  (.writeObject pem po)
-  (.flush w)
-  (.close w)
-  (.toString w))
-
-#_(.writeObject (org.bouncycastle.openssl.jcajce.JcaPEMWriter. (java.io.StringWriter.)) (.getPrivate (pem-file->private-key "./test/sample_private_key.pem")))
-
-
-#_(let [priv (pem-file->private-key "./test/sample_private_key.pem")
-      pub (pem-file->public-key "./test/sample_public_key.pem")
-      enc (encrypt "pizza" pub)]
-  (println enc)
-  (println (decrypt enc (.getPrivate
-                         priv))))
-
-;;http://stackoverflow.com/questions/7611383/generating-rsa-keys-in-pkcs1-format-in-java
--- test/block_chain/wallet_test.clj
+++ test/block_chain/wallet_test.clj
@@ -1,6 +1,7 @@
 (ns block-chain.wallet-test
   (:require [clojure.test :refer :all]
-            [block-chain.wallet :refer :all]))
+            [block-chain.wallet :refer :all]
+            [clojure.tools.namespace.repl :refer [refresh]]))
 
 ;; (def message "jFd5MRjGs3S19XIblzdkVkEwUc7A7WRPdWtj6JPXuqU6k/ue1sLRLRhc0inu\nNUglbx3TJTcq6i2FvIA1wb7LnMfw9MiFVs/wbxhtQFmx9VYKHLQq7pqKiYkZ\n6Mqtr1SiUT5e6OFc+6WEIj6GzaF6LRU9osGmUQs40iPQr5016XafRCDeIval\nNo8na5C2WZu4m4ZYadnOwDkAbuk4Vhd7xxR+F433Qitncux8oKHhVOpR9yYV\n7JzNVPzC5o+fX8PNns39Pcb91m3Z243GOl8xlfBFPYa0Wytd9DJB13MYAiWQ\n6gAGUa3kYJJIqMQU3Yyz0DCvRtoJiFXacrJoAvQ5zg==")
 
@@ -22,9 +23,9 @@
     (is (= sun.security.rsa.RSAPublicKeyImpl (type (pem-string->pub-key pub-key-pem))))
     (is (= sun.security.rsa.RSAPublicKeyImpl
            (type (pem-file->public-key "./test/sample_public_key.pem"))))
-    (is (= pub-key-pem (key->pem-string (pem-string->pub-key pub-key-pem) "PUBLIC KEY")))
+    (is (= pub-key-pem (public-key->pem-string (pem-string->pub-key pub-key-pem))))
     (is (= java.security.KeyPair
-           (type (pem-file->private-key "./test/sample_private_key.pem"))))
+           (type (pem-file->key-pair "./test/sample_private_key.pem"))))
     (is (= priv-key-pem
            (private-key->pem-string (.getPrivate (pem-file->key-pair "./test/sample_private_key.pem")))))))
 
@@ -38,7 +39,7 @@
                     priv)))))
 
 (deftest test-encrypt-and-decrypt-with-deserialized-keys
-  (let [priv (.getPrivate (pem-file->private-key "./test/sample_private_key.pem"))
+  (let [priv (.getPrivate (pem-file->key-pair "./test/sample_private_key.pem"))
         pub (pem-file->public-key "./test/sample_public_key.pem")]
     (is (= "pizza"
            (decrypt (encrypt "pizza" pub)
@@ -47,9 +48,17 @@
 (deftest test-serialize-and-deserialize-new-key
   (let [kp (generate-keypair)
         encrypted (encrypt "pizza" (.getPublic kp))
-        pem-str (private-key->pem-string (.getPrivate kp))]
+        pem-str (private-key->pem-string (.getPrivate kp))
+        deserialized (pem-string->key-pair pem-str)]
     (is (= "pizza"
            (decrypt encrypted
-                    (.getPrivate (pem-string->key-pair pem-str)))))))
+                    (.getPrivate deserialized))))))
+
+(deftest test-signing-and-verifying
+  (let [priv (.getPrivate (pem-file->key-pair "./test/sample_private_key.pem"))
+        pub (pem-file->public-key "./test/sample_public_key.pem")
+        sig (sign "pizza" priv)]
+    (is (verify sig "pizza" pub))
+    (is (not (verify sig "lul" pub)))))
 
 (run-tests)
