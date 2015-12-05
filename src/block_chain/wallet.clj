99631b74a03e16d690d55ee336038fe1e347ecdd
Horace Williams <horace.d.williams@gmail.com>
2015-12-05 10:27:23 -0700

    pulling wallet test and ns apart

--- src/block_chain/wallet.clj
+++ src/block_chain/wallet.clj
@@ -1,77 +1,12 @@
 (ns block-chain.wallet
   (:require [clojure.java.io :as io]
-            [clojure.string :refer [join split]]))
+            [clojure.string :refer [join split]]
+            [block-chain.base64 :refer :all]))
 
 ;; Thanks to http://nakkaya.com/2012/10/28/public-key-cryptography/
 ;; for many of these snippets
 
 (java.security.Security/addProvider (org.bouncycastle.jce.provider.BouncyCastleProvider.))
-
-(defn decode64 [str] (.decode (java.util.Base64/getDecoder) str))
-
-(defn encode64 [bytes] (.encodeToString (java.util.Base64/getEncoder) bytes))
-
-(defn keydata [reader]
- (->> reader
-      (org.bouncycastle.openssl.PEMParser.)
-      (.readObject)))
-
-(defn pem-string->key-pair [string]
-  "Convert a PEM-formatted private key string to a public/private keypair.
-   Returns java.security.KeyPair."
-  (let [kd (keydata (io/reader (.getBytes string)))]
-    (.getKeyPair (org.bouncycastle.openssl.jcajce.JcaPEMKeyConverter.) kd)))
-
-(defn pem-string->pub-key [string]
-  "Convert a PEM-formatted public key string to an RSA public key.
-   Returns sun.security.rsa.RSAPublicKeyImpl"
-  (let [kd (keydata (io/reader (.getBytes string)))
-        kf (java.security.KeyFactory/getInstance "RSA")
-        spec (java.security.spec.X509EncodedKeySpec. (.getEncoded kd))]
-    (.generatePublic kf spec)))
-
-(defn pem-file->public-key [filepath]
-  "Read a file containing a PEM-formatted public key
-   and return a matching key"
-  (pem-string->pub-key (slurp filepath)))
-
-(defn pem-file->key-pair [filepath]
-  "Read a file containing a PEM-formatted private key and
-   return a matching keypair"
-  (pem-string->key-pair (slurp filepath)))
-
-(defn format-pem-string [encoded key-type]
-  "Takes a Base64-encoded string of key data and formats it
-   for file-output following openssl's convention of wrapping lines
-   at 64 characters and appending the appropriate header and footer for
-   the specified key type"
-  (let [chunked (->> encoded
-                     (partition 64 64 [])
-                     (map #(apply str %)))
-        formatted (join "\n" chunked)]
-    (str "-----BEGIN " key-type "-----\n"
-         formatted
-         "\n-----END " key-type "-----\n")))
-
-(defn private-key->pem-string [key]
-  "Convert RSA private keypair to a formatted PEM string for saving in
-   a .pem file. By default these private keys will encode themselves as PKCS#8
-   data (e.g. when calling (.getEncoded private-key)), so we have to convert it
-   to ASN1, which PEM uses (this seems to also be referred to as PKCS#1).
-   More info here http://stackoverflow.com/questions/7611383/generating-rsa-keys-in-pkcs1-format-in-java"
-  (-> (.getEncoded key)
-      (org.bouncycastle.asn1.pkcs.PrivateKeyInfo/getInstance)
-      (.parsePrivateKey)
-      (.toASN1Primitive)
-      (.getEncoded)
-      (encode64)
-      (format-pem-string "RSA PRIVATE KEY")))
-
-(defn public-key->pem-string [key]
-  "Generate PEM-formatted string for a public key. This is simply a base64
-   encoding of the key wrapped with the appropriate header and footer."
-  (format-pem-string (encode64 (.getEncoded key))
-                     "PUBLIC KEY"))
 
 (defn kp-generator [length]
   (doto (java.security.KeyPairGenerator/getInstance "RSA" "BC")
@@ -119,3 +54,5 @@
               (.update msg-data))]
     (.verify sig signature)))
 
+
+(defn sign-txn [txn])
--- test/block_chain/wallet_test.clj
+++ test/block_chain/wallet_test.clj
@@ -1,33 +1,11 @@
 (ns block-chain.wallet-test
   (:require [clojure.test :refer :all]
             [block-chain.wallet :refer :all]
+            [block-chain.pem :refer [pem-file->key-pair
+                                     pem-file->public-key
+                                     private-key->pem-string
+                                     pem-string->key-pair]]
             [clojure.tools.namespace.repl :refer [refresh]]))
-
-;; (def message "jFd5MRjGs3S19XIblzdkVkEwUc7A7WRPdWtj6JPXuqU6k/ue1sLRLRhc0inu\nNUglbx3TJTcq6i2FvIA1wb7LnMfw9MiFVs/wbxhtQFmx9VYKHLQq7pqKiYkZ\n6Mqtr1SiUT5e6OFc+6WEIj6GzaF6LRU9osGmUQs40iPQr5016XafRCDeIval\nNo8na5C2WZu4m4ZYadnOwDkAbuk4Vhd7xxR+F433Qitncux8oKHhVOpR9yYV\n7JzNVPzC5o+fX8PNns39Pcb91m3Z243GOl8xlfBFPYa0Wytd9DJB13MYAiWQ\n6gAGUa3kYJJIqMQU3Yyz0DCvRtoJiFXacrJoAvQ5zg==")
-
-;; (let [pubkey (read-pem-public-key-from-string sample-pub-key-string)
-;;       encrypted (encrypt (.getBytes "Hi Josh!") pubkey)]
-;;   (println "Encrypted 'Hi Josh!':")
-;;   (println (encode64 encrypted)))
-
-(deftest test-base-64
-  (let [b (.getBytes "pizza")
-        chars (map char "pizza")]
-    (is (= chars (map char (decode64 (encode64 b)))))
-    (is (= "cGl6emE=" (encode64 (.getBytes "pizza"))))
-    (is (= chars (map char (decode64 "cGl6emE="))))))
-
-(deftest test-reading-keys
-  (let [pub-key-pem (slurp "./test/sample_public_key.pem")
-        priv-key-pem (slurp "./test/sample_private_key.pem")]
-    (is (= sun.security.rsa.RSAPublicKeyImpl (type (pem-string->pub-key pub-key-pem))))
-    (is (= sun.security.rsa.RSAPublicKeyImpl
-           (type (pem-file->public-key "./test/sample_public_key.pem"))))
-    (is (= pub-key-pem (public-key->pem-string (pem-string->pub-key pub-key-pem))))
-    (is (= java.security.KeyPair
-           (type (pem-file->key-pair "./test/sample_private_key.pem"))))
-    (is (= priv-key-pem
-           (private-key->pem-string (.getPrivate (pem-file->key-pair "./test/sample_private_key.pem")))))))
 
 (deftest test-encrypt-and-decrypt-with-fresh-keys
   (let [kp (generate-keypair)
@@ -60,5 +38,3 @@
         sig (sign "pizza" priv)]
     (is (verify sig "pizza" pub))
     (is (not (verify sig "lul" pub)))))
-
-(run-tests)
