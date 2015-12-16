4ed2f95d6ed5d03c94a86b2b3a10dbcaef83ecdc
Horace Williams <horace.d.williams@gmail.com>
2015-12-16 14:54:53 -0700

    fill in basic chain functions

--- src/block_chain/chain.clj
+++ src/block_chain/chain.clj
@@ -1,25 +1,29 @@
 (ns block-chain.chain
-  (:require [clojure.java.io :as io]))
+  (:require [clojure.java.io :as io]
+            [block-chain.utils :refer :all]))
 
 (def chain-path (str (System/getProperty "user.home")
-                     "/.block_chain.txt"))
+                     "/.block_chain.json"))
 
-(defn read-stored-chain []
-  (if (.exists (io/as-file chain-path))
-    []
-    []))
+(defn read-stored-chain
+  ([] (read-stored-chain chain-path))
+  ([path] (if (.exists (io/as-file path))
+            (into [] (read-json (slurp path)))
+            [])))
 
-(defonce block-chain (atom (read-stored-chain)))
+(def block-chain (atom []))
 
-(defn add-block [])
+(defn load-chain! []
+  (reset! block-chain (read-stored-chain)))
 
-(defn block-by-hash [hash] )
+(defn write-chain!
+  ([] (write-chain! chain-path))
+  ([path] (spit path (write-json @block-chain))))
 
-(defn block-by-height [i])
+(defn add-block! [b]
+  (swap! block-chain conj b))
 
-;; saving mined blocks
-;; need to store them somewhere -- block-chain ns?
-;; block-chain functions
-;; -- add block
-;; -- find block by hash
-;; -- find block by height
+(defn block-by-hash
+  ([hash] (block-by-hash hash @block-chain))
+  ([hash c]
+   (first (filter #(= hash (:hash %)) c))))
--- test/block_chain/chain_test.clj
+++ test/block_chain/chain_test.clj
@@ -1,7 +1,34 @@
 (ns block-chain.chain-test
   (:require [clojure.test :refer :all]
             [block-chain.chain :refer :all]
+            [block-chain.utils :refer :all]
             [clojure.tools.namespace.repl :refer [refresh]]))
+
+(def sample-block
+  {:header {:parent-hash "0"
+            :transactions-hash "tx_hash"
+            :target "F000000000000000000000000000000000000000000000000000000000000000"
+            :timestamp 1450057326
+            :nonce 0
+            }
+   :transactions []
+   :hash "some-hash"})
 
 (deftest test-reads-stored-chain
   (is (= [] (read-stored-chain))))
+
+(deftest adds-to-chain
+  (let [c (count @block-chain)]
+    (add-block! {})
+    (is (= (inc c) (count @block-chain)))))
+
+(deftest test-reads-blocks-from-json-file
+  (let [c (read-stored-chain "./test/sample_chain.json")]
+    (is (vector? c))
+    (is (= 1 (count c)))
+    (is (= "tx_hash" (get-in c [0 :header :transactions-hash])))))
+
+(deftest get-block-by-hash
+  (let [c (read-stored-chain "./test/sample_chain.json")
+        b (block-by-hash "some-hash" c)]
+    (is (= "tx_hash" (get-in b [:header :transactions-hash])))))
