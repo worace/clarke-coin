72aaed0ee7b21b117d1740bd478e9938c8f4697a
Horace Williams <horace.d.williams@gmail.com>
2016-01-07 21:04:41 -0700

    first crude pass at some echo handling

--- src/block_chain/net.clj
+++ src/block_chain/net.clj
@@ -1,21 +1,60 @@
 (ns block-chain.net
-  (:require [aleph.tcp :as tcp]
-            [manifold.stream :as s]
-            [clojure.core.async :as async]))
+  (:require #_[aleph.tcp :as tcp]
+            #_[manifold.stream :as s]
+            [clojure.java.io :as io]
+            [clojure.core.async :as async])
+  (:import  [java.net ServerSocket]))
 
 
-(defn echo-handler [s info]
-  (s/connect s s))
+;; (defn echo-handler [s info]
+;;   (s/connect s s))
 
-(defn server [port]
-  (tcp/start-server echo-handler {:port 10001}))
+;; (defn server [port]
+;;   (tcp/start-server echo-handler {:port 10001}))
 
-(defn client [host port]
-  @(tcp/client {:host host :port port}))
+;; (defn client [host port]
+;;   @(tcp/client {:host host :port port}))
 
-(defn listen [client handler]
-  (async/go
-    (loop [resp @(s/take! client)]
-      (println "got resp: " (apply handler resp))
-      (recur @(s/take! client)))
-    (println "stopping listen loop")))
+;; (defn listen [client handler]
+;;   (async/go
+;;     (loop [resp @(s/take! client)]
+;;       (println "got resp: " (apply handler resp))
+;;       (recur @(s/take! client)))
+;;     (println "stopping listen loop")))
+
+
+(defn gets
+  "Read a line of textual data from the given socket"
+  [reader]
+  (.readLine reader))
+
+(defn write-response
+  "Send the given string message out over the given socket"
+  [socket msg]
+  (let [writer (io/writer socket)]
+      (.write writer msg)
+      (.flush writer)))
+
+(defn serve [port handler]
+  (with-open [server-sock (ServerSocket. port)
+              socket (.accept server-sock)
+              stream (io/reader socket)]
+    (loop [lines []
+           next-line (gets stream)]
+      (if (empty? next-line)
+        (write-response socket (handler lines))
+        (recur (conj lines next-line)
+               (gets stream))))))
+
+
+(defn try-it []
+  (serve 8334 (fn [lines]
+                (println "handling lines: " lines)
+                (first lines))))
+
+;;echo handler:
+#_(serve 9000 (partial apply str))
+#_(let [msg-in (receive sock)
+          msg-out (handler msg-in)]
+      (write-response sock msg-out))
+
