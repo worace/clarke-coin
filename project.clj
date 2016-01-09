db164159ff84509585d9409d3db1f1fbd577f517
Horace Williams <horace.d.williams@gmail.com>
2016-01-08 19:10:14 -0700

    more tcp experiments

--- project.clj
+++ project.clj
@@ -8,6 +8,7 @@
                  [org.bouncycastle/bcpkix-jdk15on "1.53"]
                  [org.clojure/math.numeric-tower "0.0.4"]
                  [cheshire "5.5.0"]
+                 [net.async/async "0.1.0"]
                  [aleph "0.4.1-beta2"]
                  [manifold "0.1.1"]
                  [org.clojure/core.async "0.2.374"]
--- src/block_chain/net.clj
+++ src/block_chain/net.clj
@@ -1,6 +1,7 @@
 (ns block-chain.net
-  (:require #_[aleph.tcp :as tcp]
-            #_[manifold.stream :as s]
+  (:require [aleph.tcp :as tcp]
+            [net.async.tcp :as nettcp]
+            [manifold.stream :as s]
             [clojure.java.io :as io]
             [clojure.core.async :as async])
   (:import  [java.net ServerSocket]))
@@ -80,9 +81,6 @@
           (println "caught exception: " (.getMessage e)))))))
 
 
-(try
-     (/ 1 0)
-     (catch Exception e (str "caught exception: " (.getMessage e))))
 (defn handler [lines]
   (println "handling lines: " lines)
   (first lines))
@@ -93,3 +91,36 @@
           msg-out (handler msg-in)]
       (write-response sock msg-out))
 
+#_(fn [sock info]
+                      (println "sock: " sock)
+                      (println "info: " info)
+                      (s/consume #(println %) sock)
+                      (s/put! sock "hello")
+                      (s/close! sock))
+
+;; (def server
+;;   (tcp/start-server #_(fast-echo-handler inc)
+;;                     (fn [sock info]
+;;                       (prn "hi")
+;;                       (s/consume (fn [arg] (do
+;;                                              (println "consuming: " @arg)
+;;                                              (s/put! sock @arg)))
+;;                                  sock))
+;;                     {:port 8335}))
+
+;; (let [c @(tcp/client {:host "localhost" :port 8335})]
+;;   @(s/put! c "hi")
+;;   (println "client receiving:")
+;;   (println (apply str (map char @(s/take! c)))))
+
+;; (.close server)
+
+(def server (nettcp/accept (nettcp/event-loop) {:port 9988}))
+
+(let [conn (async/<!! (:accept-chan server))]
+  (println "got conn: " conn)
+  (loop [msg (async/<!! conn)]
+    (if msg
+      (do (println msg)
+          (recur (async/<!! conn)))
+      "closed")))
