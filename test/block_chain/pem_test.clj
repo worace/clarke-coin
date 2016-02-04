1c0fbb3a0bfd21ccb691559a6315b8ed0ad54cab
Horace Williams <horace.d.williams@gmail.com>
2016-02-03 18:13:18 -0700

    basic der-reading of pub key

--- test/block_chain/pem_test.clj
+++ test/block_chain/pem_test.clj
@@ -13,3 +13,10 @@
            (type (pem-file->key-pair "./test/sample_private_key.pem"))))
     (is (= priv-key-pem
            (private-key->pem-string (.getPrivate (pem-file->key-pair "./test/sample_private_key.pem")))))))
+
+
+
+
+(deftest test-reading-der
+  (let [der-string "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQqVq0QLLUg6OQM1VBuijPCc\nxml3NWMqGmtDhwHahFIGe/9Dx22f7x+jVDKXMXKJbvbchldhx+t0kcpFMHCs\ndJlfz0LhCu/M5mMFScBZht0TnYxoQnWOEoArgDrRyVU3nkexzAQHwWtH0x6P\nBh1SQOu4ZDZlQU2qOGV6gTj0dg+W8QIDAQAB\n"]
+    (is (der-string->pub-key der-string))))
