4893df5712ffde14a1ca931f3692d022faa95f0e
Horace Williams <horace.d.williams@gmail.com>
2016-05-22 09:45:57 -0600

    wip continuing down the DB testing path
    - add a few file helpers to help clean up all the temp dirs we are
      making
    - start simplifying some of the vars this txn test was using
    - still need to go back and reconcile some of our DB testing setup
      in the other tests now that i'm starting to see slightly better how
      to do it

--- src/block_chain/test_helper.clj
+++ src/block_chain/test_helper.clj
@@ -1,4 +1,5 @@
 (ns block-chain.test-helper
+  (:import [java.util UUID])
   (:require [block-chain.db :as db]
             [clj-leveldb :as ldb]
             [block-chain.queries :as q]))
@@ -25,3 +26,26 @@
 
 (defn close-conns! [& dbs]
   (doseq [db dbs] (close-conn! db)))
+
+;; File helpers thanks to clojure cookbook
+(defn safe-delete [file-path]
+  (if (.exists (clojure.java.io/file file-path))
+    (try
+      (clojure.java.io/delete-file file-path)
+      (catch Exception e (str "exception: " (.getMessage e))))
+    false))
+
+(defn delete-directory [directory-path]
+  (let [directory-contents (file-seq (clojure.java.io/file directory-path))
+        files-to-delete (filter #(.isFile %) directory-contents)]
+    (doseq [file files-to-delete]
+      (safe-delete (.getPath file)))
+    (safe-delete directory-path)))
+
+
+(defn temp-db-conn []
+  (let [path (str "/tmp/" (UUID/randomUUID))]
+    (clear-db-path! path)
+    (.addShutdownHook (Runtime/getRuntime)
+                      (Thread. #(delete-directory path)))
+    (db/conn path)))
