b4ffc9552d1ac34713585eee12c50026dbcfd9ae
Horace Williams <horace.d.williams@gmail.com>
2016-05-25 16:34:41 -0600

    remove unused env key

--- profiles.clj
+++ profiles.clj
@@ -1,6 +1,4 @@
 {:dev  {:env {:default-target "0000100000000000000000000000000000000000000000000000000000000000"
-              :db-path "dev-resources/db"
-              :db-dir "dev-resources"}}
+              :db-path "/tmp/clarke-coin-dev-db"}}
  :test {:env {:default-target "1100000000000000000000000000000000000000000000000000000000000000"
-              :db-dir "/tmp"
               :db-path "/tmp/clarke-coin-test-db"}}}
