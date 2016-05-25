34943b318cce7eb49f92b0dd3933a89a535d3e3a
Horace Williams <horace.d.williams@gmail.com>
2016-05-25 16:34:28 -0600

    clean out some old comments

--- src/block_chain/db.clj
+++ src/block_chain/db.clj
@@ -7,29 +7,7 @@
             [clj-leveldb :as ldb]
             ))
 
-;; DB namespace
-;; * collect various atom / ref types from project
-;; * remove implicit local references to stateful atoms
-;;   in varous namespaces
-;; * rewrite any atom-dependent functions to take data types
-;;   as explicit inputs
-;; * return matching data structure as output? OR take actual
-;;   atom and perform the transaction within the function
-
-;; Data Atoms:
-;; * chain.clj block-chain - (atom [])
-;; * peers.clj - peers - (atom #{})
-;; * transactions.clj - transactions -  (atom #{})
-
-;; Config / Switches
-;; * core.clj - running? - (atom true) (clean this out)
-;; * miner.clj - mine? (atom true)
-;; * net.clj - server - (atom nil)
-
 (def genesis-block (read-json (slurp (io/resource "genesis.json"))))
-
-(def chain-path (str (System/getProperty "user.home")
-                     "/.block_chain.json"))
 
 (defn json->bytes [val]
   (-> val
